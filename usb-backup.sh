cd882ab58fe43edc28fe2b9af84f1081ca7d547e
Alex Antonchev <antontchev@gmail.com>
2025-02-20 08:20:05 +0400

    the log entry is designed as a function write_log()

--- usb-backup.sh
+++ usb-backup.sh
@@ -4,10 +4,14 @@
 # The script is executed by a user with root rights (sudoers)
 # Required utilities: mount, tar, gzip, tee
 
+function write_log() {
+  echo -e "$1" | tee -a $BACKUP_PATH/$LOG_FILE
+}
+
 SOURCE_PATH=/path/to/usb-drive
 MOUNT_POINT=/path/to/mount/point
 BACKUP_PATH=$MOUNT_POINT/path/to/dir/backup # The path to the backup directory at the mount point
-BACKUP_FILE=usb.$(date +%Y-%m-%d).tar.gz # Creating a file name for the current date
+BACKUP_FILE=usb.$(date +%Y-%m-%d).tar.gz    # Creating a file name for the current date
 LOG_FILE=archive.log
 UUID=01DB2557286B1350 # UUID backup disk (for mount command)
 
@@ -18,19 +22,20 @@ if [[ ! -e $BACKUP_PATH ]]; then
 fi
 
 # Log entry datetime identifier
-echo -e "\n*** $(date +%Y-%m-%d' '%H:%S) ***\n" >>$BACKUP_PATH/$LOG_FILE
+write_log "\n*** $(date +%Y-%m-%d' '%H:%S) ***\n"
 
 # If the archive for the current date exists,
 # then write to the log and exit with the code 2
-[[ -e $BACKUP_PATH/$BACKUP_FILE ]] && echo "File $BACKUP_FILE already exists \
-in directory $BACKUP_PATH", skip | tee -a $BACKUP_PATH/$LOG_FILE && exit 2
+[[ -e $BACKUP_PATH/$BACKUP_FILE ]] &&
+  write_log "File $BACKUP_FILE already exists in directory $BACKUP_PATH, skip" &&
+  exit 2
 
-echo "Creating archive $BACKUP_FILE" >>$BACKUP_PATH/$LOG_FILE
+write_log "Creating archive $BACKUP_FILE"
 
 # Run tar and check result code
 if tar -czvf $BACKUP_PATH/"$BACKUP_FILE" -g $BACKUP_PATH/usb.snar \
   -X $BACKUP_PATH/.backupignore $SOURCE_PATH/* 2>>$BACKUP_PATH/$LOG_FILE; then
-  echo "Create arhive success" >>$BACKUP_PATH/$LOG_FILE
+  write_log "Create arhive success"
 else
-  echo "Error during create archive" >>$BACKUP_PATH/$LOG_FILE
+  write_log "Error during create archive"
 fi
