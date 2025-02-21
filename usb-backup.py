c331e7351dd75a168d4952a28e0246376542750d
Alex Antonchev <antontchev@gmail.com>
2025-02-21 07:23:28 +0400

    refactor: change variable names

--- usb-backup.py
+++ usb-backup.py
@@ -5,15 +5,17 @@ from datetime import datetime
 from getpass import getpass
 import subprocess
 
-source_path = '/media/antonchev/USB-накопитель'
-mount_point = '/mnt/store'
-backup_path = mount_point + '/Backup'
+source_path = '/path/to/usb-drive'
+mount_point = '/path/to/mount/point'
+backup_dir = f'{mount_point}/path/to/backup/dir'
 backup_file = f'usb.{datetime.strftime(datetime.today(), "%Y-%m-%d")}.tar.gz'
+backup_path = f'{backup_dir}/{backup_file}'
+
 cmd_mount = ['sudo', '-S', 'mount', '-U', '01DB2557286B1350', '/mnt/store', '-o', 'uid=1000,gid=1000']
 cmd_arch = ['tar',
-   '-czvf', backup_path + '/' + backup_file,
-    '-g', backup_path + '/usb.snar',
-    '-X', backup_path + '/.backupignore', source_path + '/*']
+   '-czvf', backup_path,
+    '-g', backup_dir + '/usb.snar',
+    '-X', backup_dir + '/.backupignore', source_path + '/*']
 
 if not os.path.ismount(mount_point):
     password = getpass("Enter your password: ")
@@ -27,10 +29,10 @@ if not os.path.ismount(mount_point):
     except subprocess.CalledProcessError as err:
         print(err.__str__())
     else:
-        if os.path.exists(f'{backup_path}/{backup_file}'):
+        if os.path.exists(f'{backup_path}'):
             print(f'Архив {backup_file} уже существует')
         else:
-            print(f'Создаем архив {backup_path}/{backup_file}')
+            print(f'Создаем архив {backup_file}')
             try:
                 proc = subprocess.run(
                     cmd_arch,
