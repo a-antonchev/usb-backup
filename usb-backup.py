c59ee20271fc935259bc1e51598541c50430093d
Alex Antonchev <antontchev@gmail.com>
2025-02-22 09:08:20 +0400

    add logging

--- usb-backup.py
+++ usb-backup.py
@@ -4,11 +4,13 @@ import os.path
 from datetime import datetime
 from getpass import getpass
 import subprocess
+import logging
 
 source_path = '/path/to/usb-drive/'
 mount_point = '/path/to/mount/point'
 backup_dir = f'{mount_point}/path/to/backup/dir'
-backup_file = f'usb.{datetime.strftime(datetime.today(), "%Y-%m-%d")}.tar.gz'
+today = datetime.strftime(datetime.today(), "%Y-%m-%d")
+backup_file = f'usb.{today}.tar.gz'
 backup_path = f'{backup_dir}/{backup_file}'
 
 cmd_mount = ['sudo', '-S', 'mount', '-U', '01DB2557286B1350', '/mnt/store', '-o', 'uid=1000,gid=1000']
@@ -34,11 +36,19 @@ if not os.path.ismount(mount_point):
         print(err.__str__())
         raise SystemExit(1)
 
+log_file = f'{backup_dir}/archive.log'
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger('backup')
+file_handler = logging.FileHandler(log_file)
+logger.addHandler(file_handler)
+
+logger.info(f'\n*** {today} ***\n')
+
 if os.path.exists(f'{backup_path}'):
-    print(f'Архив {backup_file} уже существует')
+    logger.info(f'File {backup_file} already exists in directory, skip.')
     raise SystemExit(2)
 else:
-    print(f'Создаем архив {backup_path}/{backup_file}')
+    logger.info(f'Creating archive {backup_path}/{backup_file}.')
     try:
         proc = subprocess.run(
             cmd_arch,
@@ -46,5 +56,5 @@ else:
             stderr=subprocess.PIPE,
             check=True)
     except subprocess.CalledProcessError as err:
-        print(err.__str__())
+        logger.info(err.__str__())
         raise SystemExit(3)
